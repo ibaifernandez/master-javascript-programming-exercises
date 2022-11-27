2a8aa6f13985b9c178331422afc421ac9259a6f8
Ibai Fernández <info@ibaifernandez.com>
2022-11-27 12:21:37 +0000

    Solve Ex. #150

--- exercises/150-flipEveryNChars/app.js
+++ exercises/150-flipEveryNChars/app.js
@@ -1,7 +1,25 @@
 function flipEveryNChars(input, n) {
-    // your code here
+  const arr = input.split('')
+  let smallArr = [];
+  const finalArr = [];
+
+  console.log(arr);
+
+for (let i = 0; i < arr.length; i += n) {
+      const chunk = arr.slice(i, i + n);
+      smallArr.push(chunk.reverse());
+}
+
+  for (let j = 0; j < smallArr.length; j++) {
+    finalArr.push(smallArr[j].join(''));
+  }
+  return finalArr.join('');
 }
 
 let input = 'a short example';
 let output = flipEveryNChars(input, 5);
 console.log(output); // --> ohs axe trelpma
+  
+let output2 = flipEveryNChars('watch my backflip', 3);
+console.log(output2); // --> taw hc ymcablfkpi
+  
